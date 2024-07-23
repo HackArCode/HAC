ed6018e3589fda209d92ddd2f52684b1a46cb9a7
ivedmohan <vedmohan0@gmail.com>
2024-07-23 14:02:50 +0530

    animation

---
+++ script.js
@@ -0,0 +1,46 @@
+function homePageAnimation(){
+    gsap.set(".slidesm",{scale: 5})
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".home",
+        start:"top top",
+        end: "bottom end",
+        pin: true,
+        scrub: 2
+    },
+});
+tl
+.to(".vdio",{
+    '--golu': "0%",
+    ease: Power2,
+},'x')
+.to(".slidesm",{
+    scale: 1,
+    ease: Power2,
+},'x')
+.to(".lft",{
+    xPercent: -5,
+    stagger: 0.3,
+    ease: Power4,
+},'b')
+.to(".rgt",{
+    xPercent: 5,
+    stagger: 0.3,
+    ease: Power4,
+},'b')
+}
+
+// gsap.to(".slides",{
+//     scrollTrigger: {
+//         trigger: ".prize",
+//         start: "top top",
+//         end: "bottom bottom",
+//         markers: true,
+//         scrub: 1
+//     },
+//     xPercent: -200,
+//     ease: Power4.ease
+    
+// })
+
+homePageAnimation();
